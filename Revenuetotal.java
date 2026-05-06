732216b063174d6ff1de7e170db2aff068d241c8
jaya <jayasreebanda510@gmail.com>
2026-05-06 20:55:58 +0530

    resolved negative pending values and improved payment tracking logic

--- Revenuetotal.java
+++ Revenuetotal.java
@@ -1,19 +1,23 @@
 package com.vehicleToll;
 
-public class Revenuetotal extends Payment{
-static void fullCompletedVehiclesPayament() {
-	System.out.println();
-	System.out.println("-----TOTAL REVENUE-------");
-	System.out.println("Vehicles List Completed Payment 		  : "+vehiclesCompletedPayment);
-	System.out.println("Count of Vehicles Payment Done 			  : "+countvehiclesPaymentDone);
-	System.out.println("Total Revenue On Completed Vehicles Payment: "+totalCompletedvehiclesPayment);
+public class Revenuetotal extends Payment {
+	static void fullCompletedVehiclesPayament() {
+		System.out.println();
+		System.out.println("-----TOTAL REVENUE-------");
+		System.out.println("Vehicles List Completed Payment 		  : " + vehiclesCompletedPayment);
+		System.out.println("Count of Vehicles Payment Done 			  : " + countvehiclesPaymentDone);
+		System.out.println("Total Revenue On Completed Vehicles Payment: " + totalCompletedvehiclesPayment);
 
-}
-static void incompletedVehiclesList() {
-	if((int)unCompleteVehiclesPaymentTotal!=0) {
-	System.out.println(vehiclesPaymentInComplete);
-	System.out.println(unCompleteVehiclesPaymentTotal);
-	
-}
-}
+	}
+
+	static void inCompletedVehiclesPayemtPendingList() {
+		if (suspendedPendingvehicles!=0) {
+			System.out.println();
+			System.out.println("-----TOTAL REVENUE-------");
+			System.out.println("Vehicles List Pending Payment 		  : " + suspendedPendingvehiclesPayment);
+			System.out.println("Count of Vehicles Pending Payment Done 			  : " + suspendedPendingvehicles);
+			System.out.println("Total Revenue Pending Payment Vehicles: " + remainingPerVehiclePayment);
+
+		}
+	}
 }
