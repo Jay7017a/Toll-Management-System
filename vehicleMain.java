b3d74913fc1cebadc644a8001312601968455fe7
jaya <jayasreebanda510@gmail.com>
2026-05-06 20:53:10 +0530

    streamlined vehicle input loop control based on user response

--- vehicleMain.java
+++ vehicleMain.java
@@ -8,6 +8,7 @@ public class vehicleMain extends Toll{
 	static String vehicleNumber;
 	static String vehicletype;
 	static double entryTime;
+	static String  nextVehicleInQueue;
 	static void addVehicle() {
 		System.out.println("Enter Vehicle Number : ");
 		vehicleNumber=sc.next();
@@ -21,8 +22,9 @@ public class vehicleMain extends Toll{
 		
 		System.out.println();
 		System.out.println("========================================");
-
+		String againEnterVehicle="no";
 		System.out.println("TOLL MANAGEMENT SYSTEM");
+	do {
 	
 		System.out.println("=========================================");
 		vehicleMain.addVehicle();
@@ -31,8 +33,25 @@ public class vehicleMain extends Toll{
 		v1.getVehicleDetails();
 		v1.vehicleTypecheck(vehicletype,entryTime);
 		v1.selectPaymentMode(v1.vehicletype, v1.tollTotal,v1.vehicleNumber);
-		Revenuetotal.fullCompletedVehiclesPayament();
-		
-		
+		//Revenuetotal.fullCompletedVehiclesPayament();
+		System.out.println("Next Vehicle is There");
+		nextVehicleInQueue=sc.next();
+		if(nextVehicleInQueue.equalsIgnoreCase("yes")) {
+			if(totamount==tollTotal)
+				againEnterVehicle="yes";
+			
 		}
+		else if ((nextVehicleInQueue.equalsIgnoreCase("yes"))){
+			againEnterVehicle="yes";
+		}
+		else {
+			againEnterVehicle="no";
+		}
+	}
+	
+	while(againEnterVehicle.equalsIgnoreCase("yes"));
+	Revenuetotal.fullCompletedVehiclesPayament();
+	System.out.println();
+	Revenuetotal.inCompletedVehiclesPayemtPendingList();
+}
 }
